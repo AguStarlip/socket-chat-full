59c32fd565e36ba9c4a26927226611ab22fbf880
AguStarlip <lipchak09gmail.com>
2020-08-26 19:00:07 -0300

    beta smart-chat

--- public/js/socket-chat.js
+++ public/js/socket-chat.js
@@ -4,21 +4,22 @@ var params = new URLSearchParams(window.location.search);
 
 if (!params.has('nombre') || !params.has('sala')) {
     window.location = 'index.html';
-    throw new Error('El nombre y sala son necesario');
+    throw new Error('El nombre y sala son necesarios');
 }
 
 var usuario = {
     nombre: params.get('nombre'),
     sala: params.get('sala')
-}
+};
+
+
 
 socket.on('connect', function() {
     console.log('Conectado al servidor');
 
     socket.emit('entrarChat', usuario, function(resp) {
-
-        console.log('Usuarios conectados', resp);
-
+        /* console.log('Usuarios conectados', resp); */
+        renderizarUsuarios(resp);
     });
 
 });
@@ -32,31 +33,29 @@ socket.on('disconnect', function() {
 
 
 // Enviar información
-socket.emit('enviarMensaje', {
-    usuario: 'Fernando',
-    mensaje: 'Hola Mundo'
-}, function(resp) {
-    console.log('respuesta server: ', resp);
-});
+// socket.emit('crearMensaje', {
+//     nombre: 'Fernando',
+//     mensaje: 'Hola Mundo'
+// }, function(resp) {
+//     console.log('respuesta server: ', resp);
+// });
 
 // Escuchar información
 socket.on('crearMensaje', function(mensaje) {
-
-    console.log('Servidor:', mensaje);
-
+    /* console.log('Servidor:', mensaje); */
+    renderizarMensajes(mensaje, false);
+    scrollBottom();
 });
 
 // Escuchar cambios de usuarios
-// entrada y salida del chat
-socket.on('listaPersonas', function(personas) {
-
-    console.log(personas);
-
+// cuando un usuario entra o sale del chat
+socket.on('listaPersona', function(personas) {
+    renderizarUsuarios(personas);
 });
 
 // Mensajes privados
 socket.on('mensajePrivado', function(mensaje) {
 
-    console.log('Mensaje Privado: ', mensaje);
+    console.log('Mensaje Privado:', mensaje);
 
-})
+});
